2cebcb629c3355684f30b34f1cb6240cd3a9148b
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-10-05 19:02:49 -0400

    Added Bonus section, multiprocessing, new and improved plots.

--- piantadosi2016/graphs.py
+++ piantadosi2016/graphs.py
@@ -4,9 +4,13 @@ import colorsys
 
 from bokeh import io as bkio
 from bokeh import plotting as bkp
+from bokeh import models as bkm
+
 from bokeh.core.properties import value
 from bokeh.models import FixedTicker
 from bokeh.models.mappers import LinearColorMapper
+from bokeh.plotting import ColumnDataSource
+from bokeh.models import HoverTool
 
 def interact(*args, **kwargs):
     widgets.interact(*args, **kwargs)
@@ -137,10 +141,10 @@ palette_20 = ["#%02x%02x%02x" % rgb for rgb in palette_20]
 
 def fig2a(D, x_max=30, y_max=10, palette=palette_20,
           title='P(survive to adulthood)', show=True,
-          plot_width=500, plot_height=400):
-    fig = bkp.figure(x_range=(0, x_max), y_range=(0, y_max), webgl=True,
+          plot_width=500, plot_height=400, tools=''):
+    fig = bkp.figure(x_range=(0, x_max), y_range=(0, y_max), webgl=False,
                      plot_width=plot_width, plot_height=plot_height,
-                     tools='pan,wheel_zoom,reset,save', title=title)
+                     tools=tools, title=title)
     fig.title.text_font_size = '12pt'
     fig.title.align = 'center'
     tweak_fig(fig, tree_ticks_enable=False, grid=False)
@@ -149,6 +153,11 @@ def fig2a(D, x_max=30, y_max=10, palette=palette_20,
     Dmat = fig.image(image=[D], x=0, y=0, dw=x_max, dh=y_max, color_mapper=cmap)
     fig.xaxis.axis_label = "Birth age (month)"
     fig.yaxis.axis_label = "Brain size (radius, cm)"
+
+    color_bar = bkm.ColorBar(color_mapper=cmap, label_standoff=12, location=(0,0),
+                             border_line_color=None, bar_line_color='black', major_tick_line_color='black')
+
+    fig.add_layout(color_bar, 'right')
 
     if show:
         handle = bkp.show(fig, notebook_handle=True)
@@ -173,15 +182,70 @@ palette_2 = ['#dddddd', '#ffffff']
 def line_color():
     """Return a random (bright) color"""
     r, g, b = colorsys.hsv_to_rgb(np.random.random(), 0.65, 0.95)
-    return (255*r, 255*g, 255*b)
+    return '#{0:02x}{1:02x}{2:02x}'.format(int(255*r), int(255*g), int(255*b))
 
-def fig2b(D, traces, x_max=30, y_max=10):
-    handle, fig, data = fig2a(D, x_max=30, y_max=10, palette=palette_2, show=False,
-                              plot_width=900, plot_height=720, title='Population dynamics')
+
+def fig2b(traces, D, x_max=30, y_max=10):
+    return fig2b_aux(traces, D, x_max=30, y_max=10, axes=('T', 'R'),
+                     line_width=3, radius=0.2)
+
+def intelligence_radius_fig(traces):
+    return fig2b_aux(traces, None, x_max=11, y_max=10, axes=('I', 'R'),
+                     line_width=1, radius=0.05, display_D=False,
+                     display_start=False, display_end=True)
+
+def fig2b_aux(traces, D, x_max=30, y_max=10, axes=('T', 'R'),  line_width=3, radius=0.2,
+              display_D=True, display_start=True, display_end=False):
+
+    x_idx = ('R', 'T', 'I').index(axes[0])
+    y_idx = ('R', 'T', 'I').index(axes[1])
+
+    if display_D:
+        handle, fig, data = fig2a(D, x_max=x_max, y_max=y_max, palette=palette_2, show=False,
+                                  plot_width=900, plot_height=720, title='Population dynamics',
+                                  tools='pan,wheel_zoom,reset,save')
+    else:
+        fig = bkp.figure(x_range=(0, x_max), y_range=(0, y_max), webgl=False,
+                         plot_width=660, plot_height=600,
+                         tools='pan,wheel_zoom,reset,save', title='Intelligence/Brain radius relationship')
+        tweak_fig(fig, tree_ticks_enable=False, grid=False)
+
+    fig.xaxis.axis_label = ["Brain size (radius, cm)", "Birth age (month)", "Intelligence"][x_idx]
+    fig.yaxis.axis_label = ["Brain size (radius, cm)", "Birth age (month)", "Intelligence"][y_idx]
+
+    colors = []
     for trace in traces:
         color = line_color()
-        R_0, T_0 = trace[0][:2]
-        fig.circle([T_0], [R_0], radius=0.2, color=color)
-        fig.line([RTI[1] for RTI in trace], [RTI[0] for RTI in trace],
-                 line_width=3, line_join='bevel', color=color)
+        colors.append(color)
+        fig.line([RTI[x_idx] for RTI in trace], [RTI[y_idx] for RTI in trace],
+                 line_width=line_width, line_join='bevel', color=color)
+
+    source = ColumnDataSource(
+        data=dict(
+            x=[trace[0][x_idx] for trace in traces],
+            y=[trace[0][y_idx] for trace in traces],
+            x_end=[trace[-1][x_idx] for trace in traces],
+            y_end=[trace[-1][y_idx] for trace in traces],
+            R_start=['{:.2f}'.format(trace[0][0]) for trace in traces],
+            T_start=['{:.2f}'.format(trace[0][1]) for trace in traces],
+            I_start=['{:.2f}'.format(trace[0][2]) for trace in traces],
+            R_end=['{:.2f}'.format(trace[-1][0]) for trace in traces],
+            T_end=['{:.2f}'.format(trace[-1][1]) for trace in traces],
+            I_end=['{:.2f}'.format(trace[-1][2]) for trace in traces],
+        )
+    )
+    if display_start:
+        g_r = fig.circle('x', 'y', source=source, radius=radius, color=colors)
+    if display_end:
+        g_r = fig.square('x_end', 'y_end', source=source, size=10, color=colors)
+
+    g_hover = bkm.HoverTool(renderers=[g_r],
+                            tooltips=[("R_start", "@R_start"),
+                                      ("R_end",   "@R_end"),
+                                      ("T_start", "@T_start"),
+                                      ("T_end",   "@T_end"),
+                                      ("I_start", "@I_start"),
+                                      ("I_end",   "@I_end"),
+                                     ])
+    fig.add_tools(g_hover)
     bkp.show(fig)
